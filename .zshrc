87623064af18e6418f324a5ddbfe4e534c5c854e
griffin baker <griffin_baker12@icloud.com>
2024-10-06 12:24:20 -0500

    update stuff

--- .zshrc
+++ .zshrc
@@ -1,13 +1,1 @@
-alias py="python3"
-autoload -Uz vcs_info
-precmd() { vcs_info }
-zstyle ':vcs_info:git:*' formats '[%b]'
-setopt PROMPT_SUBST
-PROMPT='%~ ${vcs_info_msg_0_}$ '
-export CLICOLOR=1
-export PS1=$'%n@%m:\e[0;36m%~\e[0m$ '
-export EDITOR='vim'
-bindkey -e
-alias ggpush="git push"
-alias venv='[ ! -d "venv" ] && py -m venv venv;source venv/bin/activate'
-alias ods='vim ~/dotfiles'
+/Users/griff/.zshrc
--- scripts/create_sym_links.sh
+++ scripts/create_sym_links.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
-
-DOTFILES_DIR="$HOME/dotfiles"
-DOTFILES_BACKUP_DIR="$HOME/dotfile_backups"
-
-# Function to create symlink
-create_symlink() {
-    local src="$1"
-    local dest="$2"
-    if [ -e "$dest" ]; then
-        if [ ! -L "$dest" ]; then
-            mv "$dest" "${dest}.backup"
-            echo "Backed up existing $dest to ${dest}.backup"
-        fi
-    fi
-    ln -sf "$src" "$dest"
-    echo "Created symlink: $dest -> $src"
-}
-
-# Create symlinks
-create_symlink "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
-create_symlink "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
-create_symlink "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
-
-echo "Dotfiles setup complete!"
+ln -s ~/.vimrc ~/dotfiles/.vimrc
+ln -s ~/.zshrc ~/dotfiles/.zshrc
+ln -s ~/.tmux.conf ~/dotfiles/.tmux.conf
