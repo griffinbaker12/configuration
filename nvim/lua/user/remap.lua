7191b4d5787f327fb55c70c1884250105979e5b3
griffin baker <griffin_baker12@icloud.com>
2024-10-16 00:48:53 -0500

    oops

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -58,5 +58,5 @@ vim.keymap.set(
 	{ noremap = true, silent = true }
 )
 
-vim.keymap.set({ "n", "v" }, "<leader>x", [["+x]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
