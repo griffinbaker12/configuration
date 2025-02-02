24942668baf4410f0ed59d6a25138fa33aa5978b
griffin baker <griffin_baker12@icloud.com>
2025-02-01 23:41:55 -0800

    cool

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,57 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		-- Add files to list
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Harpoon: Add file" })
+		vim.keymap.set("n", "<leader>A", function()
+			harpoon:list():prepend()
+		end, { desc = "Harpoon: Prepend file" })
+
+		-- Toggle quick menu
+		-- vim.keymap.set("n", "<leader>h", function()
+		vim.keymap.set("n", "<M-y>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon: Toggle menu" })
+
+		-- Navigation (keeping your Alt-based scheme)
+		-- vim.keymap.set("n", "<M-y>", function()
+		-- 	harpoon:list():select(1)
+		-- end, { desc = "Harpoon: File 1" })
+		vim.keymap.set("n", "<M-u>", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon: File 1" })
+		vim.keymap.set("n", "<M-i>", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon: File 2" })
+		vim.keymap.set("n", "<M-o>", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon: File 3" })
+		vim.keymap.set("n", "<M-p>", function()
+			harpoon:list():select(4)
+		end, { desc = "Harpoon: File 4" })
+
+		-- Add replace functionality with Alt+Shift+key
+		vim.keymap.set("n", "<M-Y>", function()
+			harpoon:list():replace_at(1)
+		end, { desc = "Harpoon: Replace 1" })
+		vim.keymap.set("n", "<M-U>", function()
+			harpoon:list():replace_at(2)
+		end, { desc = "Harpoon: Replace 2" })
+		vim.keymap.set("n", "<M-I>", function()
+			harpoon:list():replace_at(3)
+		end, { desc = "Harpoon: Replace 3" })
+		vim.keymap.set("n", "<M-O>", function()
+			harpoon:list():replace_at(4)
+		end, { desc = "Harpoon: Replace 4" })
+		vim.keymap.set("n", "<M-P>", function()
+			harpoon:list():replace_at(5)
+		end, { desc = "Harpoon: Replace 5" })
+	end,
+}
